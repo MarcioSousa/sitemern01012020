939bb99c26117ed80dd6efb91ddfbaf21c183e42
MarcioSousa <marciofrapeso@gmail.com>
2020-12-20 14:09:22 -0300

    Criado Tela index com botão para Login

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -1,16 +1,18 @@
-// cSpell:Ignore Cabecalho
+// cSpell:Ignore Cabecalho, Empresa
 import React from 'react'
 
 import AppBar from '@material-ui/core/AppBar'
 import Typography from '@material-ui/core/Typography'
 import Toolbar from '@material-ui/core/Toolbar'
-import ApartmentIcon from '@material-ui/icons/Apartment'
+import Button from '@material-ui/core/Button'
 import { makeStyles} from '@material-ui/core/styles'
+
+import ApartmentIcon from '@material-ui/icons/Apartment'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 
 const useStyles = makeStyles((theme) => ({
     toolbarTitle: {
         flex: 1
-
     }
 }))
 
@@ -28,6 +30,11 @@ const Cabecalho = () => {
                     className={classes.toolbarTitle}>
                         Empresa Delta
                     </Typography>
+                    <Button variant='contained'
+                    startIcon={<LockOutlinedIcon/>}
+                    color='secondary'
+                    size='small'
+                    href='/login'>Login</Button>
                 </Toolbar>
             </AppBar>
         </>
